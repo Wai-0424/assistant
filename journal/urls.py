1cff54270bd178940d3fe51e764d7934414666b2
Wai-0424 <yw930424@gmail.com>
2020-01-02 12:00:41 +0800

    美化

--- journal/urls.py
+++ journal/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', JournalList.as_view()), 
-    path('create/', JournalCreate.as_view()),
+    path('', JournalList.as_view(),name='journal_list'), 
+    path('create/', JournalCreate.as_view(),name='journal_create'),
     path('<int:pk>/update/', JournalUpdate.as_view()), 
     path('<int:pk>/delete/', JournalDelete.as_view()),
 ]
